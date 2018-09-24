5fb5cca0f86d4c6ef53a165ccd724116e26a6e8d
sivanotc <sivanotc@CLASS42>
2018-09-24 17:45:04 +0300

    first commit

--- TestSite/UnitTest1.cs
+++ TestSite/UnitTest1.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace TestSite
 {
@@ -22,8 +23,31 @@ namespace TestSite
             Assert.AreEqual(chrome.FindElementsByClassName("product-layout").Count,count);
         }
 
+        [TestCase("Apple")]
+        public void TestCategoryDropDown(string search)
+        {
+            chrome = new ChromeDriver();
+
+            chrome.Navigate().GoToUrl("http://atqc-shop.epizy.com/");
+            chrome.FindElementByName("search").SendKeys(search);
+            chrome.FindElementByClassName("fa-search").Click();
+            chrome.FindElementByName("category_id").Click();
+
+            for (int i = 0; i < chrome.FindElementsByTagName("option").Count; i++) {
+                chrome.FindElementsByTagName("option")[i].Click();
+                Assert.AreEqual(current.Text, chrome.FindElementsByTagName("option")[i].Text);
+            }
+
+            foreach (var current in chrome.FindElementsByTagName("option")) {
+                current.Click();
+                
+                
+            }
+            
+        }
+
         [TestCase("Apple", 4, 17)]
-        public void TestCategoryDropDown(string search, int count, int categoryIndex) {
+        public void TestCategoryResult(string search, int count, int categoryIndex) {
             chrome = new ChromeDriver();
 
             chrome.Navigate().GoToUrl("http://atqc-shop.epizy.com/");
@@ -35,6 +59,7 @@ namespace TestSite
 
             Assert.AreEqual(chrome.FindElementsByClassName("product-layout").Count, count);
         }
+
 
 
         [TearDown]
