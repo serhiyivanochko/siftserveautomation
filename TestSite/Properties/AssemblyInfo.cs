2c285647d06b5a24c985218b6373b6818d52e148
sivanotc <sivanotc@CLASS42>
2018-09-24 17:15:49 +0300

    first commit

---
+++ TestSite/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestSite")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestSite")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("54c07673-aa64-4117-980f-702c07a6be1c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestSite/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TestSite
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        ChromeDriver chrome;
+
+        [TestCase("Apple",7)]
+        public void SearchingResultItemsCount(string search, int count)
+        {
+            chrome = new ChromeDriver();
+
+            chrome.Navigate().GoToUrl("http://atqc-shop.epizy.com/");
+            chrome.FindElementByName("search").SendKeys(search);
+            chrome.FindElementByClassName("fa-search").Click();
+
+            Assert.AreEqual(chrome.FindElementsByClassName("product-layout").Count,count);
+        }
+
+        [TestCase("Apple", 4)]
+        public void TestCategoryDropDown(string search, int count) {
+            chrome = new ChromeDriver();
+
+            chrome.Navigate().GoToUrl("http://atqc-shop.epizy.com/");
+            chrome.FindElementByName("search").SendKeys(search);
+            chrome.FindElementByClassName("fa-search").Click();
+            chrome.FindElementByName("category_id").Click();
+            chrome.FindElementsByTagName("option")[17].Click();
+            chrome.FindElementById("button-search").Click();
+
+            Assert.AreEqual(chrome.FindElementsByClassName("product-layout").Count, count);
+        }
+
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            chrome.Close();
+        }
+
+    }
+}
