54dd0a32d89f689893410dc08062c84f218bfa93
sivanotc <sivanotc@CLASS42>
2018-09-27 19:28:21 +0300

    ""

--- TestSite/Pages/Content.cs
+++ TestSite/Pages/Content.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
 
 namespace TestSite.Pages
 {
     class Content
     {
-
+        protected IWebElement searchLabel { get; private set; }
+        protected IWebElement searchTextBox { get; private set; }
+        protected IWebElement searchCategoryCheck { get; private set; }
+        protected IWebElement searchDescriptionChek { get; private set; }
+        protected IWebElement searchButton { get; private set; }
+        protected SelectElement selectCategory { get; private set; }
     }
 }
