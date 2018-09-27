03e306cad7bbc2b6e0712070e23562a05ceaa005
sivanotc <sivanotc@CLASS42>
2018-09-27 19:42:16 +0300

    ""

--- TestSite/Pages/Content.cs
+++ TestSite/Pages/Content.cs
@@ -26,7 +26,24 @@ namespace TestSite.Pages
         protected SelectElement selectSortBy { get; private set; }
         protected SelectElement selectShow { get; private set; }
 
+        public Content(IWebDriver driver)
+        {
 
+            this.searchLabel = driver.FindElement(By.CssSelector(".content+h1"));
+            this.searchTextBox = driver.FindElement(By.Id("input-search"));
+            this.searchCategoryCheck = driver.FindElement(By.Name("sub_category"));
+            this.searchDescriptionChek = driver.FindElement(By.Name("description"));
+            this.searchButton = driver.FindElement(By.Id("button-search"));
+            this.listShowButton = driver.FindElement(By.Id("list-view"));
+            this.gridShowButton = driver.FindElement(By.Id("grid-view"));
+            this.productCompareLabel = driver.FindElement(By.Id("compare-total"));
+            this.productPageLabel = driver.FindElement(By.ClassName("text-right"));
+
+            this.selectCategory = new SelectElement(driver.FindElement(By.Name("category_id")));
+            this.selectSortBy = new SelectElement(driver.FindElement(By.Id("input-sort")));
+            this.selectShow = new SelectElement(driver.FindElement(By.Id("input-limit")));
+
+        }
 
     }
 }
